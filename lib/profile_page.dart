779d0b6054298b10a2304766090aabe438cf0db9
aly <y307@ukr.net>
2026-06-23 14:06:50 +0300

    In profile_page added ListTitle

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app3/notifiers.dart';
+import 'package:flutter_app3/wellcome_page.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text('Profile page'),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          ListTile(
+            title: const Text('Logout'),
+            onTap: () {
+              selectedPageNotifier.value = 0;
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return WellcomePage();
+                  },
+                ),
+              ); // Navigator.pushReplacementNamed(context, '/settings');
+            },
+          ),
+        ],
+      ),
     );
   }
 }
