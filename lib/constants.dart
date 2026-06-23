ae7e4402e94f8ce68cf534a52d996d07d190793c
aly <y307@ukr.net>
2026-06-24 00:52:45 +0300

    Card and costants TextStyle

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KTextStyle {
+  static final TextStyle titleTealText = TextStyle(
+    color: Colors.blue,
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+  static final TextStyle descriptionText = TextStyle(fontSize: 16);
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app3/constants.dart';
 import 'package:flutter_app3/hero_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,11 +8,33 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: const Padding(
-        padding: EdgeInsets.all(20.0),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            HeroWidget(),
+            const HeroWidget(),
+            // Padding(
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Card(
+                color: Colors.amber[50],
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    crossAxisAlignment:
+                        CrossAxisAlignment.start, // смещение текста влево
+                    children: [
+                      Text('Basic Layout', style: KTextStyle.titleTealText),
+                      Text(
+                        'The description of this',
+                        style: KTextStyle.descriptionText,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
